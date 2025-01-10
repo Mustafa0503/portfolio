9fb0d7deb96a0f144bb354c8ebe935b31ba0d10e
Yashank Bhola <yashank.bhola@mail.utoronto.ca>
2025-01-09 22:01:08 -0500

    biryani 7

--- vite.config.js
+++ vite.config.js
@@ -7,16 +7,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      },
-      output: {
-        entryFileNames: `assets/[name].[hash].js`,
-        chunkFileNames: `assets/[name].[hash].js`,
-        assetFileNames: `assets/[name].[hash].[ext]`
-      }
-    },
   },
   resolve: {
     extensions: ['.js', '.jsx']
