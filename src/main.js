ba489f2ae4f9148a86b2387c489fcb3139134ab4
Yashank Bhola <yashank.bhola@mail.utoronto.ca>
2025-01-09 21:26:46 -0500

    chicken biryani

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-//Let's Roll
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -8,4 +7,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+); 
